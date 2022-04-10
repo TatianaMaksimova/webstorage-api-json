4a4f95d3775676d8e84abb08544f02c3bf5a0ed7
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-04-10 23:26:40 +0300

    finishes colorpicker.js

--- src/js/colorpicker.js
+++ src/js/colorpicker.js
@@ -1,8 +1,16 @@
+const Handlebars = require('handlebars');
+
 // import colorCardTpl from '../templates/color-card.hbs';
 // import colorCardsTpl from '../templates/color-cards.hbs';
+// import template from '../templates/color-card.hbs';
 import colors from './colors.json';
 import '../css/common.css';
 import '../css/colorpicker.css';
+
+const template = Handlebars.compile(
+  '{{#each this}}<div class="color-card"><div class="color-swatch" data-hex="{{hex}}" data-rgb="{{rgb}}"style="background-color: {{hex}}"></div><div class="color-meta"><p>HEX: {{hex}}</p><p>RGB: {{rgb}}</p></div></div>{{/each}}',
+);
+// console.log(template({ hex: '#f44336', rgb: '244,67,54' }));
 
 const paletteContainer = document.querySelector('.js-palette');
 const cardsMarkup = createColorCardsMarkup(colors);
@@ -13,9 +21,9 @@ paletteContainer.addEventListener('click', onPaletteContainerClick);
 
 function createColorCardsMarkup(colors) {
   // return colors.map(color => colorCardTpl(color)).join('');
-  // return colors.map(colorCardTpl).join('');
-
-  return colorCardsTpl(colors);
+  // return colors.map(template).join('');
+  // return colorCardsTpl(colors);
+  return template(colors);
 }
 
 function onPaletteContainerClick(evt) {
